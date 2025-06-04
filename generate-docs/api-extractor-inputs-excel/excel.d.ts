84751ce4953ddbfa349a66a80c09e0909bae7e9e
Alison McKay <almckay@microsoft.com>
2025-06-04 16:49:14 -0700

    Manually add beta tags and re-run GenerateDocs.sh

--- generate-docs/api-extractor-inputs-excel/excel.d.ts
+++ generate-docs/api-extractor-inputs-excel/excel.d.ts
@@ -15195,6 +15195,7 @@ export declare namespace Global {
          * Return the text encoding of the document as a PDF.
          * If the document is empty, then the following error is shown: "We didn't find anything to print".
          * @returns The content of the workbook as a string, in PDF format.
+         * @beta
          */
         export function convertToPdf(): string;
 
@@ -15202,6 +15203,7 @@ export declare namespace Global {
          * Downloads a specified file to the default download location specified by the local machine.
          * @param name - The name of the file once downloaded. The file extension determines the type of the file. Supported extensions are ".txt" and ".pdf". Default is ".txt".
          * @param content - The content of the file.
+         * @beta
          */
         export function downloadFile({
             name,
@@ -15215,40 +15217,48 @@ export declare namespace Global {
          * Send an email with an Office Script. Use `MailProperties` to specify the content and recipients of the email.
          * If the request body includes content, this method returns 400 Bad request.
          * @param message - The properties that define the content and recipients of the email.
+         * @beta
          */
         export function sendMail(mailProperties: MailProperties): void;
 
         /**
          * The type of the content. Possible values are text or HTML.
+         * @beta
          */
         enum EmailContentType {
             /**
              * The email message body is in HTML format.
+             * @beta
              */
             html = "html",
 
             /**
              * The email message body is in plain text format.
+             * @beta
              */
             text = "text",
         }
 
         /**
          * The importance value of the email. Corresponds to "high", "normal", and "low" importance values available in the Outlook UI.
+         * @beta
          */
         enum EmailImportance {
             /**
              * Email is marked as low importance.
+             * @beta
              */
             low = "low",
 
             /**
              * Email does not have any importance specified.
+             * @beta
              */
             normal = "normal",
 
             /**
              * Email is marked as high importance.
+             * @beta
              */
             high = "high",
         }
@@ -15257,69 +15267,83 @@ export declare namespace Global {
          * The attachment to send with the email.
          * A value must be specified for at least one of the `to`, `cc`, or `bcc` parameters.
          * If no recipient is specified, the following error is shown: "The message has no recipient. Please enter a value for at least one of the "to", "cc", or "bcc" parameters."
+         * @beta
          */
         export interface EmailAttachment {
             /**
              * The text that is displayed below the icon representing the attachment. This string doesn't need to match the file name.
+             * @beta
              */
             name: string;
             /**
              * The contents of the file.
+             * @beta
              */
             content: string;
         }
 
         /**
          * The properties of the email to be sent.
+         * @beta
          */
         export interface MailProperties {
             /**
              * The subject of the email. Optional.
+             * @beta
              */
             subject?: string;
 
             /**
              * The content of the email. Optional.
+             * @beta
              */
             content?: string;
 
             /**
              * The type of the content in the email. Possible values are text or HTML. Optional.
+             * @beta
              */
             contentType?: EmailContentType;
 
             /**
              * The importance of the email. The possible values are `low`, `normal`, and `high`. Default value is `normal`. Optional.
+             * @beta
              */
             importance?: EmailImportance;
 
             /**
              * The direct recipient or recipients of the email. Optional.
+             * @beta
              */
             to?: string | string[];
 
             /**
              * The carbon copy (CC) recipient or recipients of the email. Optional.
+             * @beta
              */
             cc?: string | string[];
 
             /**
              * The blind carbon copy (BCC) recipient or recipients of the email. Optional.
+             * @beta
              */
             bcc?: string | string[];
 
             /**
              * A file (such as a text file or Excel workbook) attached to a message. Optional.
+             * @beta
              */
             attachments?: EmailAttachment | EmailAttachment[];
         }
 
         /**
          * Metadata about the script.
+         * @beta
          */
         export namespace Metadata {
             /**
              * Get the current executing scripts name.
+             * @beta
              */
             export function getScriptName(): string;
         }
