64f1cfc724d6f43ff72c0b2ced0ea7d982e21177
Alison McKay <almckay@microsoft.com>
2025-09-05 16:36:01 -0700

    Include proper beta tags

--- generate-docs/script-inputs/office-scripts-docs.d.ts
+++ generate-docs/script-inputs/office-scripts-docs.d.ts
@@ -15260,15 +15260,18 @@ declare namespace OfficeScript {
 
     /**
      * The file to download.
+     * @beta
      */
     export interface FileProperties {
         /**
          * The name of the file once downloaded. The file extension determines the type of the file. Supported extensions are ".txt" and ".pdf". Default is ".txt".
+         * @beta
          */
         name: string;
 
         /**
          * The content of the file.
+         * @beta
          */
         content: string;
     }
@@ -15287,35 +15290,42 @@ declare namespace OfficeScript {
 
     /**
      * The type of the content. Possible values are text or HTML.
+     * @beta
      */
     enum EmailContentType {
         /**
          * The email message body is in HTML format.
+         * @beta
          */
         html = "html",
 
         /**
          * The email message body is in plain text format.
+         * @beta
          */
         text = "text",
     }
 
     /**
      * The importance value of the email. Corresponds to "high", "normal", and "low" importance values available in the Outlook UI.
+     * @beta
      */
     enum EmailImportance {
         /**
          * Email is marked as low importance.
+         * @beta
          */
         low = "low",
 
         /**
          * Email does not have any importance specified.
+         * @beta
          */
         normal = "normal",
 
         /**
          * Email is marked as high importance.
+         * @beta
          */
         high = "high",
     }
@@ -15324,69 +15334,83 @@ declare namespace OfficeScript {
      * The attachment to send with the email.
      * A value must be specified for at least one of the `to`, `cc`, or `bcc` parameters.
      * If no recipient is specified, the following error is shown: "The message has no recipient. Please enter a value for at least one of the "to", "cc", or "bcc" parameters."
+     * @beta
      */
     export interface EmailAttachment {
         /**
          * The text that is displayed below the icon representing the attachment. This string doesn't need to match the file name.
+         * @beta
          */
         name: string;
         /**
          * The contents of the file.
+         * @beta
          */
         content: string;
     }
 
     /**
      * The properties of the email to be sent.
+     * @beta
      */
     export interface MailProperties {
         /**
          * The subject of the email. Optional.
+         * @beta
          */
         subject?: string;
 
         /**
          * The content of the email. Optional.
+         * @beta
          */
         content?: string;
 
         /**
          * The type of the content in the email. Possible values are text or HTML. Optional.
+         * @beta
          */
         contentType?: EmailContentType;
 
         /**
          * The importance of the email. The possible values are `low`, `normal`, and `high`. Default value is `normal`. Optional.
+         * @beta
          */
         importance?: EmailImportance;
 
         /**
          * The direct recipient or recipients of the email. Optional.
+         * @beta
          */
         to?: string | string[];
 
         /**
          * The carbon copy (CC) recipient or recipients of the email. Optional.
+         * @beta
          */
         cc?: string | string[];
 
         /**
          * The blind carbon copy (BCC) recipient or recipients of the email. Optional.
+         * @beta
          */
         bcc?: string | string[];
 
         /**
          * A file (such as a text file or Excel workbook) attached to a message. Optional.
+         * @beta
          */
         attachments?: EmailAttachment | EmailAttachment[];
     }
 
     /**
      * Metadata about the script.
+     * @beta
      */
     namespace Metadata {
         /**
          * Get the name of the currently running script.
+         * @beta
          */
         function getScriptName(): string;
     }
